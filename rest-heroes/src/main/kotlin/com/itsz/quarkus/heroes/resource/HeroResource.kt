7c774ec1942ea6fef0c5f0d87191c8794e511931
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2023-03-29 15:36:23 +0800

    feat: heroes reactive CURD api

--- rest-heroes/src/main/kotlin/com/itsz/quarkus/heroes/resource/HeroResource.kt
+++ rest-heroes/src/main/kotlin/com/itsz/quarkus/heroes/resource/HeroResource.kt
@@ -4,23 +4,32 @@ import com.itsz.quarkus.heroes.model.Hero
 import com.itsz.quarkus.heroes.service.HeroService
 import io.smallrye.mutiny.Uni
 import org.jboss.resteasy.reactive.RestPath
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.Path
+import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.*
 import javax.ws.rs.core.Response
 
 @Path("/api/heroes")
+@ApplicationScoped
 class HeroResource(val heroService: HeroService) {
-
 
     @GET
     fun getAll(): Uni<Response> = heroService.getAll().onItem().transform { Response.ok(it).build() }
 
     @GET
     @Path("/{id}")
-    fun getById(@RestPath id: Long): Uni<Response> = heroService.getById(id).onItem().transform { Response.ok(it).build() }
+    fun getById(@RestPath id: Long): Uni<Response> = heroService.getById(id)
+            .onItem()
+            .transform { it.let { if (it != null) Response.ok(it).build() else Response.status(404).build() } }
 
     @POST
-    fun createHero (hero: Hero):Uni<Response> = heroService.create(hero).onItem().transform { Response.ok(it).build() }
+    fun createHero(hero: Hero): Uni<Response> = heroService.create(hero).onItem().transform { Response.ok(it).build() }
+
+    @PUT
+    fun updateHero(hero: Hero): Uni<Response> = heroService.update(hero).onItem().transform { Response.ok(it).build() }
+
+    @DELETE
+    @Path("/{id}")
+    fun delete(@RestPath id: Long): Uni<Response> = heroService.delete(id).onItem().transform { Response.noContent().build() }
+
 
 }
--- rest-heroes/src/main/kotlin/com/itsz/quarkus/heroes/service/HeroService.kt
+++ rest-heroes/src/main/kotlin/com/itsz/quarkus/heroes/service/HeroService.kt
@@ -1,13 +1,39 @@
 package com.itsz.quarkus.heroes.service
 
 import com.itsz.quarkus.heroes.model.Hero
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional
+import io.smallrye.mutiny.Uni
+import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.BadRequestException
 
+@ApplicationScoped
 class HeroService {
 
     fun getAll() = Hero.findAll().list()
 
-    fun getById(id: Long) = Hero.findById(id)
+    fun getById(id: Long): Uni<Hero?> = Hero.findById(id)
 
+    @ReactiveTransactional
     fun create(hero: Hero) = hero.persist<Hero>()
 
+
+   @ReactiveTransactional
+    fun update(hero: Hero): Uni<Hero?> = Hero.findById(id = hero.id!!)
+            .onItem().ifNull()
+            .failWith { BadRequestException() }
+            .onItem()
+            .transform {
+                it?.let {
+                    it.name = hero.name
+                    it.otherName = hero.otherName
+                    it.picture = hero.picture
+                    it.level = hero.level
+                    it.powers = hero.powers
+                }
+                it
+            }
+
+    @ReactiveTransactional
+    fun delete(id: Long) = Hero.deleteById(id)
 }
+
